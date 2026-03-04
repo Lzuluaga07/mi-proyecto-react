5a9ac6ab93f0004a0f7aebb886b9363379e74a58
Lzuluaga07 <betiko391@gmail.com>
2026-03-04 09:15:28 -0500

    Eliminar workflow que causaba conflicto de ramas

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Proyecto-react/', 
+  base: '/Proyecto-react/', // <-- ESTO arregla el error 404 de tu imagen
 })
